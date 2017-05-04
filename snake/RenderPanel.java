cfa41b39505dfd5c9c690ba61637bf24f75a5047
Chandan Kumar Mishra <cmishra646@gmail.com>
2017-05-04 15:24:19 +0530

    Update RenderPanel.java

--- snake/RenderPanel.java
+++ snake/RenderPanel.java
@@ -8,12 +8,12 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 /**
- * @author Jaryt Bustard
+ * @author chandan mishra
  */
 public class RenderPanel extends JPanel
 {
 
-	public static final Color GREEN = new Color(1666073);
+	public static final Color BLACK = new Color(000000);
 
 	@Override
 	protected void paintComponent(Graphics g)
@@ -22,11 +22,11 @@ public class RenderPanel extends JPanel
 		
 		Snake snake = Snake.snake;
 
-		g.setColor(GREEN);
+		g.setColor(BLACK);
 		
 		g.fillRect(0, 0, 800, 700);
 
-		g.setColor(Color.BLUE);
+		g.setColor(Color.YELLOW);
 
 		for (Point point : snake.snakeParts)
 		{
@@ -39,9 +39,9 @@ public class RenderPanel extends JPanel
 		
 		g.fillRect(snake.cherry.x * Snake.SCALE, snake.cherry.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
 		
-		String string = "Score: " + snake.score + ", Length: " + snake.tailLength + ", Time: " + snake.time / 20;
-		
+		String string = "Score: " + snake.score + ", Length: " + snake.tailLength +", Time: " + snake.time / 20;
 		g.setColor(Color.white);
+
 		
 		g.drawString(string, (int) (getWidth() / 2 - string.length() * 2.5f), 10);
 
